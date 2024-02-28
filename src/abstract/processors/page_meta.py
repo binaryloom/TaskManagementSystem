6de22c2e775382ecc486f78054867c96376e29b1
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 22:16:26 +0000

    Feb 29, 2024, 4:16 AM

--- src/abstract/processors/page_meta.py
+++ src/abstract/processors/page_meta.py
@@ -2,6 +2,9 @@ from django.urls import resolve, reverse
 
 
 def meta_processor(request):
-    url_name = resolve(request.path).url_name
-    if url_name == "boardlist_view":
-        url_name = "Boards"
+    page_title = resolve(request.path).url_name
+    if page_title == "boardlist_view":
+        page_title = "Boards"
+    return {
+        "page_title": page_title,
+    }
