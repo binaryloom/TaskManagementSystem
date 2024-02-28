ea0a1533d642ff36a8077895aea6298da53e7f8c
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 12:00:52 +0000

    Feb 28, 2024, 6:00 PM

--- src/task_management/admin.py
+++ src/task_management/admin.py
@@ -1,3 +1,30 @@
+from django.conf import settings
 from django.contrib import admin
 
-# Register your models here.
+from abstract.admin import ModelAdmin
+from task_management.models import Board, List, Task
+
+
+class BoardAdmin(ModelAdmin):
+    pass
+
+
+class ListAdmin(ModelAdmin):
+    pass
+
+
+class TaskAdmin(ModelAdmin):
+    pass
+    # list_display = BaseAdmin.list_display + [
+    #     "country",
+    #     "branch_count",
+    #     "faculty_count",
+    #     "discipline_count",
+    #     "session_count",
+    # ]
+
+
+if settings.ENABLE_ADMIN:
+    admin.site.register(Board, BoardAdmin)
+    admin.site.register(List, ListAdmin)
+    admin.site.register(Task, TaskAdmin)
