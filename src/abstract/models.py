bc829cbab35e45a28d8c0cb139dce05557cba2cf
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 12:09:30 +0000

    Feb 28, 2024, 6:09 PM

--- src/abstract/models.py
+++ src/abstract/models.py
@@ -4,30 +4,8 @@ from django.db import models
 from abstract.managers import BaseManager
 
 
-class BaseModelClass(models.Model):
-    """This is the base class model for all of the database tables.
-
-    Contains:
-        created_on = Datetime of creation.
-        updated_on = Last modification datetime.
-
-    Usage:
-        class <CLASS_NAME> (BaseModelClass):
-            # model definition
-    """
-
-    created_at = models.DateTimeField(auto_now_add=True, blank=True)
-    updated_at = models.DateTimeField(auto_now=True, blank=True)
-
-    class Meta:
-        abstract = True
-        ordering = ["-updated_at"]
-        if not settings.DEFAULT_PERMISSIONS:
-            default_permissions = []
-
-
-class BaseModel(BaseModelClass):
-    """This is the base class model for all state full models of the database tables.
+class BaseModel(models.Model):
+    """This is the base class model for all  models of the database tables.
 
     Contains:
         created_on = Datetime of creation.
@@ -44,6 +22,8 @@ class BaseModel(BaseModelClass):
     objects = BaseManager()
 
     status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
 
     def delete(self, *args, **kwargs):
         self.status = False
