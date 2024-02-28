2f8a2cd91d056d6a97c3bfec92945048b0bc93dc
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 17:37:37 +0000

    Feb 28, 2024, 11:37 PM

--- src/tms/settings.py
+++ src/tms/settings.py
@@ -148,8 +148,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.DjangoModelPermissions"],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         # "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
-        # "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 12,
