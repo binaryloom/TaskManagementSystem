6e0d9c9a91455e8478621384604f2ec0bfdd14fb
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 11:00:21 +0000

    Feb 28, 2024, 5:00 PM

--- src/abstract/managers.py
+++ src/abstract/managers.py
@@ -2,4 +2,14 @@ from django.db import models
 
 
 class BaseManager(models.Manager):
-    pass
+    def get_queryset(self):
+        return super().get_queryset()
+
+    def all(self):
+        return super().all().exclude(status=False)
+
+    def filter(self, *args, **kwargs):
+        return super().filter(*args, **kwargs).exclude(status=False)
+
+    def count(self):
+        return self.filter().count()
