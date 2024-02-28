9ef86b275f080c19900cc241c6117a4b88dcb6ca
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 10:38:25 +0000

    Feb 28, 2024, 4:38 PM

--- src/abstract/enums.py
+++ src/abstract/enums.py
@@ -10,25 +10,17 @@ class CellSize(IntEnum):
         CellSize.XS: 2
         CellSize.SMALL: 3
         CellSize.MEDIUM: 20
-        CellSize.XM: 50
         CellSize.LARGE: 100
         CellSize.XL: 255
         CellSize.XXL: 500
-        CellSize.XXXL: 1000
-        CellSize.EXTRAXL: 10000
-        CellSize.EXTRAXXL: 100000
-        CellSize.EXTRAXXXL = 10485760
+        CellSize.XXXL: 10485760
     """
 
     XXS = 1
     XS = 2
     SMALL = 3
     MEDIUM = 20
-    XM = 50
     LARGE = 100
     XL = 255
     XXL = 500
-    XXXL = 1000
-    EXTRAXL = 10000
-    EXTRAXXL = 100000
-    EXTRAXXXL = 10485760
+    XXXL =  10485760
