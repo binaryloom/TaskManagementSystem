fa1077e6de2835d0b87d1a6ceaf3b5610d537ee3
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 22:17:44 +0000

    Feb 29, 2024, 4:17 AM

--- src/tms/settings.py
+++ src/tms/settings.py
@@ -87,6 +87,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "abstract.processors.meta_processor",
             ],
         },
     },
