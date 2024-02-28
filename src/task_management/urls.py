f910f7d587cf924613db433261e07ba7536a5308
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 21:10:12 +0000

    Feb 29, 2024, 3:10 AM

--- src/task_management/urls.py
+++ src/task_management/urls.py
@@ -1,8 +1,17 @@
+from django.urls import path
 from rest_framework import routers
 
+from task_management.views import DashboardView
 from task_management.viewsets import BoardViewSet, ListViewSet, TaskViewSet
 
 router = routers.DefaultRouter()
 router.register(r"board", BoardViewSet)
 router.register(r"list", ListViewSet)
 router.register(r"task", TaskViewSet)
+
+
+# fmt: off
+urlpatterns = [
+    path(route="", view=DashboardView.as_view(), name="dashboard_view"),
+]
+# fmt: on
--- src/task_management/views.py
+++ src/task_management/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from abstract.views import TemplateView
+
 # Create your views here.
+
+
+class DashboardView(TemplateView):
+    template_name = "task_management/dashboard.html"
