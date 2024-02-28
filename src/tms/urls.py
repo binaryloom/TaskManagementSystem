bb6fb5286194213cb62a8e7f47170e5740f663b7
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 12:51:09 +0000

    Feb 28, 2024, 6:51 PM

--- src/tms/urls.py
+++ src/tms/urls.py
@@ -18,7 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from task_management.urls import router
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("rest_framework.urls")),
+    path("api/task/", include(router.urls)),
 ]
