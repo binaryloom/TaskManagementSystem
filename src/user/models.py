3a867aab27ca9620d43fcbbf6ecc38cd000cdf52
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 11:34:32 +0000

    Feb 28, 2024, 5:34 PM

--- src/user/models.py
+++ src/user/models.py
@@ -1,31 +1,31 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-from versity_info.enums import CellSize, UserType
+from django.db.models import CharField
 
-from .managers import UserManager
+from abstract.enums import CellSize
+
+# from .managers import UserManager
 
 # Create your models here.
 
 
 class User(AbstractUser):
     USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["password"]
+    # REQUIRED_FIELDS = ["password"]
     # objects = UserManager()
-    mobile_no = models.CharField(
+    mobile_no = CharField(
         unique=True, max_length=CellSize.MEDIUM, null=True, blank=True
     )
 
     def __str__(self):
         return self.username
 
-    def get_full_name(self):
-        return (
-            self.username
-            if not self.first_name and self.last_name
-            else f"{self.first_name} {self.last_name}"
-        )
+    # def get_full_name(self):
+    #     return (
+    #         self.username
+    #         if not self.first_name and self.last_name
+    #         else f"{self.first_name} {self.last_name}"
+    #     )
 
     class Meta:
         managed = settings.MANAGE_DATABASE
