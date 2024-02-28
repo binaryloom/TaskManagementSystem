4445f9055d1fa2fe889262933ee0f9eebaf9c634
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 10:50:40 +0000

    Feb 28, 2024, 4:50 PM

--- src/abstract/models.py
+++ src/abstract/models.py
@@ -3,14 +3,32 @@ from abstract.managers import BaseManager
 from django.conf import settings
 
 
-# from user.models import User
-# from versity_info.enums import CellSize
-# from versity_info.abstract_managers import BaseManager
-# from phonenumber_field import modelfields
-
-
-class BaseClass(models.Model):
+class BaseModelClass(models.Model):
     """This is the base class model for all of the database tables.
+
+    Contains:
+        created_on = Datetime of creation.
+        updated_on = Last modification datetime.
+
+    Usage:
+        class <CLASS_NAME> (BaseModelClass):
+            # model definition
+    """
+
+    objects = BaseManager()
+
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-updated_at"]
+        if not settings.DEFAULT_PERMISSIONS:
+            default_permissions = []
+
+
+class BaseClass(BaseModelClass):
+    """This is the base class model for all state full models of the database tables.
 
     Contains:
         created_on = Datetime of creation.
@@ -22,15 +40,7 @@ class BaseClass(models.Model):
             # model definition
     """
 
-    operating_user = None
-    objects = BaseManager()
-
     status = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True, blank=True)
-    updated_at = models.DateTimeField(auto_now=True, blank=True)
 
-    class Meta:
-        abstract = True
-        ordering = ["-updated_at"]
-        if not settings.DEFAULT_PERMISSIONS:
-            default_permissions = []
+    class Meta(BaseModelClass.Meta):
+        pass
