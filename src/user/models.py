8b6458e4e87c22b9051c5b869e77831f20907cae
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 12:26:31 +0000

    Feb 28, 2024, 6:26 PM

--- src/user/models.py
+++ src/user/models.py
@@ -2,26 +2,21 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
 
-# from abstract.enums import CellSize
-
-# from .managers import UserManager
-
 
 class User(AbstractUser):
     USERNAME_FIELD = "username"
-    # REQUIRED_FIELDS = ["password"]
-    # objects = UserManager()
+
     mobile_no = CharField(unique=True, max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.username
 
-    # def get_full_name(self):
-    #     return (
-    #         self.username
-    #         if not self.first_name and self.last_name
-    #         else f"{self.first_name} {self.last_name}"
-    #     )
+    def get_full_name(self):
+        return (
+            self.username
+            if not self.first_name and self.last_name
+            else f"{self.first_name} {self.last_name}"
+        )
 
     def delete(self, *args, **kwargs):
         if self.is_staff:
