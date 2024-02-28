7e88085d930ee19de527ef17037fc6a02a9f8378
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 12:33:00 +0000

    Feb 28, 2024, 6:33 PM

--- src/user/admin.py
+++ src/user/admin.py
@@ -2,12 +2,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
+from abstract.admin import mark_undo
 from user.models import User
 
 
 class UserAdmin(UserAdmin):
     search_fields = ("username", "mobile_no", "email")
-    ordering = ("username", "email")
+    ordering = ("username", "email", "first_name", "last_name")
 
     fieldsets = UserAdmin.fieldsets + (("Custom Fields", {"fields": ("mobile_no",)}),)
     add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("mobile_no",)}),)
