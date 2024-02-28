08795ea9a13da84841fd1cb0d3683e2db35056e0
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 18:39:25 +0000

    Feb 29, 2024, 12:39 AM

--- src/tms/settings.py
+++ src/tms/settings.py
@@ -145,11 +145,12 @@ FIXTURE_DIRS = [Path(path.join(BASE_DIR, "fixtures"))]
 
 # Rest Framework
 REST_FRAMEWORK = {
-    # "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.DjangoModelPermissions"],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.DjangoModelPermissions"],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.BasicAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 12,
+    # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    # "PAGE_SIZE": 12,
 }
