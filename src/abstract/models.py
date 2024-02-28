fcaeda8d50c7cfb1c648b5435c086a656f592d69
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 10:45:34 +0000

    Feb 28, 2024, 4:45 PM

--- src/abstract/models.py
+++ src/abstract/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from abstract.managers import BaseManager
 
 # from user.models import User
 # from versity_info.enums import CellSize
@@ -13,8 +14,6 @@ class BaseClass(models.Model):
     Contains:
         created_on = Datetime of creation.
         updated_on = Last modification datetime.
-        created_by = Created by which user.
-        updated_by = User who lastly modified.
         status = Status of a record active or not.
 
     Usage:
@@ -28,15 +27,3 @@ class BaseClass(models.Model):
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
-    created_by = models.ForeignKey(
-        User,
-        related_name="created_by_%(class)s",
-        on_delete=models.DO_NOTHING,
-        blank=True,
-    )
-    updated_by = models.ForeignKey(
-        User,
-        related_name="updated_by_%(class)s",
-        on_delete=models.DO_NOTHING,
-        blank=True,
-    )
