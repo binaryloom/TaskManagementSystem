7ff157578c735ccdb09b971e87a36b7031fa77f9
fiery.snowflake <extinctCoder@outlook.com>
2024-02-28 19:52:21 +0000

    Feb 29, 2024, 1:52 AM

--- src/tms/urls.py
+++ src/tms/urls.py
@@ -31,13 +31,15 @@ from user.urls import router as user_router
 from user.views import RegistrationView
 
 urlpatterns = [
-    # path("auth/", include("rest_framework.urls", namespace="basic_auth")),
-    path("auth/registration", register, name="register"),
+    path("auth/login/", login, name="login"),
+    path("auth/logout/", logout, name="logout"),
+    path("auth/profile/", profile, name="profile"),
+    path("auth/register/", register, name="register"),
+    path("auth/reset_password/", reset_password, name="reset_password"),
     path("auth/jwt/", TokenObtainPairView.as_view(), name="jwt_auth"),
     path("auth/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
     path("auth/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("auth/registration/", RegistrationView.as_view(), name="registration"),
-    path("accounts/", include("rest_registration.api.urls")),
 ]
 
 
@@ -53,9 +55,3 @@ urlpatterns = urlpatterns + [
 urlpatterns = urlpatterns + [
     path("admin/", admin.site.urls),
 ]
-
-
-# login
-# logout
-# profile
-# change Password
