aedae381d1c5520a843376c648a316d4ba764462
fiery.snowflake <extinctCoder@outlook.com>
2024-02-29 15:27:49 +0000

    Feb 29, 2024, 9:27 PM

--- src/task_management/forms.py
+++ src/task_management/forms.py
@@ -1,2 +1,17 @@
 from abstract.forms import ModelForm
 from task_management.models import Board, List, Task
+
+
+class BoardForm(ModelForm):
+    class Meta:
+        model = Board
+
+
+class ListForm(ModelForm):
+    class Meta:
+        model = List
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
