110bb4de5b5ce65993bdfda2995f47f9f60ede01
fiery.snowflake <extinctCoder@outlook.com>
2024-02-29 16:56:07 +0000

    Feb 29, 2024, 10:56 PM

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework import routers
 
-from user.views import DashboardView
+from user.views import DashboardView, LoginView, LogoutView
 from user.viewsets import GroupViewSet, UserViewSet
 
 router = routers.DefaultRouter()
@@ -12,5 +12,7 @@ router.register(r"group", GroupViewSet)
 # fmt: off
 urlpatterns = [
     path(route="", view=DashboardView.as_view(), name="dashboard_view"),
+    path(route="login", view=LoginView.as_view(), name="login_view"),
+    path(route="logout", view=LogoutView.as_view(), name="logout_view"),
 ]
 # fmt: on
