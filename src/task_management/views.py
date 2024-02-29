99c77ee4018db349b1e75049795309a80fec3f89
fiery.snowflake <extinctCoder@outlook.com>
2024-02-29 16:51:36 +0000

    Feb 29, 2024, 10:51 PM

--- src/task_management/views.py
+++ src/task_management/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import render
 
-from abstract.views import (
-    CreateView,
-    DetailChildView,
-    ListView,
-    TemplateView,
-    UpdateView,
-)
+from abstract.views import CreateView, DetailChildView, ListView, UpdateView
 from task_management.forms import BoardForm, ListForm, TaskForm
 from task_management.models import Board, List, Task
-
-
-class DashboardView(TemplateView):
-    template_name = "task_management/dashboard.html"
 
 
 class BoardListView(ListView):
