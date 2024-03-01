5a0f310e64d8a36aa412ec2465143841a4fbcee7
fiery.snowflake <extinctCoder@outlook.com>
2024-03-01 10:50:03 +0000

    Mar 1, 2024, 4:50 PM

--- src/abstract/forms.py
+++ src/abstract/forms.py
@@ -3,5 +3,13 @@ from django import forms
 
 class ModelForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop("user", None)
+        print("-----------------------------------------")
+        print(user)
+        super().__init__(*args, **kwargs)
+        # if user is not None:
+        #     self.fields['user'].queryset = Book.objects.filter(user=user)
+
     class Meta:
         exclude = ["status", "created_by", "updated_by"]
