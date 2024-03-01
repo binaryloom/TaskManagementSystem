6825514fb054ab22b377f387f81e932493b0ab00
fiery.snowflake <extinctCoder@outlook.com>
2024-03-01 09:53:52 +0000

    Mar 1, 2024, 3:53 PM

--- src/abstract/views.py
+++ src/abstract/views.py
@@ -42,6 +42,9 @@ class DetailChildView(DetailView):
     template_name = "default/detail_child.html"
     field = None
     child_header = None
+    filter_by_user = True
+
+    def get_objects ()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
