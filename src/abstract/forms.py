d4912405b61b08c138a8ed77f1d376ed7f64388a
fiery.snowflake <extinctCoder@outlook.com>
2024-03-01 11:02:27 +0000

    Mar 1, 2024, 5:02 PM

--- src/abstract/forms.py
+++ src/abstract/forms.py
@@ -10,17 +10,28 @@ class ModelForm(forms.ModelForm):
         print(self._meta.model)
 
         fields = self._meta.model._meta.get_fields()
-
+        # task_management.Task.id
+        # task_management.Task.status
+        # task_management.Task.created_at
+        # task_management.Task.updated_at
+        # task_management.Task.created_by
+        # task_management.Task.updated_by
+        # task_management.Task.title
+        # task_management.Task.description
+        # task_management.Task.assigned_list
+        # task_management.Task.due_date
+        # task_management.Task.assigned_to
         for field in fields:
-            if field.is_relation:
+            print(field)
+            # if field.is_relation:
+            #     model_str = f"{field.related_model._meta.app_label}.{field.related_model._meta.object_name}"
+            #     print("field_name", field.name)
 
-                model_str = f"{field.related_model._meta.app_label}.{field.related_model._meta.object_name}"
-                print("field_name", field.name)
-
-                print("related model", model_str)
+            #     print("related model", model_str)
         super().__init__(*args, **kwargs)
         # if user is not None:
         #     self.fields['user'].queryset = Book.objects.filter(user=user)
 
     class Meta:
+        exclude_models = ["user.User"]
         exclude = ["status", "created_by", "updated_by"]
