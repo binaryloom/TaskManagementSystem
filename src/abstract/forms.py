c0f30eeb8445bc001172f6fc36dc885397aadae1
fiery.snowflake <extinctCoder@outlook.com>
2024-03-01 11:23:27 +0000

    Mar 1, 2024, 5:23 PM

--- src/abstract/forms.py
+++ src/abstract/forms.py
@@ -6,48 +6,16 @@ class ModelForm(forms.ModelForm):
     exclude_models = ["user.User"]
 
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop("user", None)
-        print("-----------------------------------------")
-        # print(user)
-        # print(self._meta.model)
-
-        # fields = self._meta.model._meta.get_fields()
-        # task_management.Task.id
-        # task_management.Task.status
-        # task_management.Task.created_at
-        # task_management.Task.updated_at
-        # task_management.Task.created_by
-        # task_management.Task.updated_by
-        # task_management.Task.title
-        # task_management.Task.description
-        # task_management.Task.assigned_list
-        # task_management.Task.due_date
-        # task_management.Task.assigned_to
-
+        super().__init__(*args, **kwargs)
         for field in self._meta.model._meta.get_fields():
             if (
                 isinstance(field, ForeignKey)
                 and f"{field.related_model._meta.app_label}.{field.related_model._meta.object_name}"
                 not in self.exclude_models
             ):
-
-                model_str = f"{field.related_model._meta.app_label}.{field.related_model._meta.object_name}"
-                # for field in fields:
-                print(field, model_str)
-
                 self.fields[field.name].queryset = self.fields[
-                    "author"
-                ].queryset.filter(created_by=user)
-        # if field.is_relation:
-        #     model_str = f"{field.related_model._meta.app_label}.{field.related_model._meta.object_name}"
-        #     print("field_name", field.name)
-
-        #     print("related model", model_str)
-        print("-----------------------------------------")
-        super().__init__(*args, **kwargs)
-        # if user is not None:
-        #     self.fields['user'].queryset = Book.objects.filter(user=user)
+                    field.name
+                ].queryset.filter(created_by=kwargs.pop("user", None))
 
     class Meta:
-
         exclude = ["status", "created_by", "updated_by"]
