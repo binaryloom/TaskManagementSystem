a1b93fc09a342f7a352628adbc5b2cd724a740aa
fiery.snowflake <extinctCoder@outlook.com>
2024-03-01 10:53:04 +0000

    Mar 1, 2024, 4:53 PM

--- src/abstract/forms.py
+++ src/abstract/forms.py
@@ -8,6 +8,12 @@ class ModelForm(forms.ModelForm):
         print("-----------------------------------------")
         print(user)
         print(self._meta.model)
+
+        fields = self._meta.model._meta.get_fields()
+
+        for field in fields:
+            if field.is_relation and field.many_to_one:
+                print(field.name)
         super().__init__(*args, **kwargs)
         # if user is not None:
         #     self.fields['user'].queryset = Book.objects.filter(user=user)
