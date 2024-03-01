807d98604ba95245a936058bda89208df5ee9b73
fiery.snowflake <extinctCoder@outlook.com>
2024-03-01 10:26:21 +0000

    Mar 1, 2024, 4:26 PM

--- src/abstract/forms.py
+++ src/abstract/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class ModelForm(forms.ModelForm):
-
-    class Meta:
-        exclude = ["status", "created_by", "updated_by"]
+    pass
+    # class Meta:
+    #     exclude = ["status", "created_by", "updated_by"]
--- src/task_management/forms.py
+++ src/task_management/forms.py
@@ -3,20 +3,20 @@ from task_management.models import Board, List, Task
 
 
 class BoardForm(ModelForm):
-    class Meta(ModelForm.Meta):
+    class Meta:
         model = Board
         fields = "__all__"
 
 
 class ListForm(ModelForm):
 
-    class Meta(ModelForm.Meta):
+    class Meta:
         model = List
         fields = "__all__"
 
 
 class TaskForm(ModelForm):
 
-    class Meta(ModelForm.Meta):
+    class Meta:
         model = Task
         fields = "__all__"
