cd97bc627b892c7d6dba81e5cd76adb3e7bb90a3
fiery.snowflake <extinctCoder@outlook.com>
2024-03-01 09:48:28 +0000

    Mar 1, 2024, 3:48 PM

--- src/abstract/views.py
+++ src/abstract/views.py
@@ -48,7 +48,9 @@ class DetailChildView(DetailView):
         if self.field and hasattr(context["object"], self.field):
             if self.child_header:
                 context["child_header"] = self.child_header
-            context["object_list"] = getattr(context["object"], self.field).all()
+            context["object_list"] = getattr(context["object"], self.field).filter(
+                created_by=self.request.user
+            )
         return context
 
 
