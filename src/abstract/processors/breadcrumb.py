b443938c3ca4c38b4f0f7964f2eb20657195706d
fiery.snowflake <extinctCoder@outlook.com>
2024-03-01 11:46:01 +0000

    Mar 1, 2024, 5:46 PM

--- src/abstract/processors/breadcrumb.py
+++ src/abstract/processors/breadcrumb.py
@@ -1,5 +1,6 @@
 def breadcrumb(request):
-    breadcrumb = []
+    # breadcrumb = []
+    breadcrumb = [{"title": "Home", "link": "/"}]
     if current_link := request.path[1:]:
         current_link = current_link[:-1] if current_link[-1] == "/" else current_link
         link_segments = "/"
@@ -8,6 +9,5 @@ def breadcrumb(request):
                 {"title": fragment, "link": link_segments + str(fragment)}
             )
             link_segments += f"{str(fragment)}/"
-    else:
-        breadcrumb = [{"title": "Home", "link": "/"}]
+
     return {"breadcrumb": breadcrumb}
