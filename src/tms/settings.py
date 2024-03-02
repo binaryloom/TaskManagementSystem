93ed4520d4566f0d5a1eb7a2982db1c5fa6ecc74
fiery.snowflake <extinctCoder@outlook.com>
2024-03-02 21:30:59 +0000

    Mar 3, 2024, 3:30 AM

--- src/tms/settings.py
+++ src/tms/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    # "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     # custom app
     "tms",
