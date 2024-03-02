cbbe21475f4a5abe99c424692c54700ab8a8d9f8
fiery.snowflake <extinctCoder@outlook.com>
2024-03-02 14:12:33 +0000

    Mar 2, 2024, 8:12 PM

--- src/task_management/serializers.py
+++ src/task_management/serializers.py
@@ -3,18 +3,36 @@ from task_management.models import Board, List, Task
 
 
 class BoardSerializer(HyperlinkedModelSerializer):
+    """
+    Serializer for Board model.
+
+    This serializer serializes Board instances to JSON representations.
+    """
+
     class Meta:
         model = Board
         fields = ["url", "name"]
 
 
 class ListSerializer(HyperlinkedModelSerializer):
+    """
+    Serializer for List model.
+
+    This serializer serializes List instances to JSON representations.
+    """
+
     class Meta:
         model = List
         fields = ["url", "name", "assigned_board"]
 
 
 class TaskSerializer(HyperlinkedModelSerializer):
+    """
+    Serializer for Task model.
+
+    This serializer serializes Task instances to JSON representations.
+    """
+
     class Meta:
         model = Task
         fields = [
