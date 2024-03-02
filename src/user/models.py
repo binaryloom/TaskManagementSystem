b48c65763652bbb09b1fe84172ff37e26fa3dbaa
fiery.snowflake <extinctCoder@outlook.com>
2024-03-02 14:42:44 +0000

    Mar 2, 2024, 8:42 PM

--- src/user/models.py
+++ src/user/models.py
@@ -4,14 +4,54 @@ from django.db.models import CharField
 
 
 class User(AbstractUser):
+    """
+    Custom user model.
+
+    This class extends the default AbstractUser provided by Django to customize user behavior and attributes.
+
+    Attributes:
+        USERNAME_FIELD (str): The field used for user authentication, set to "username".
+        mobile_no (CharField): Field for storing the user's mobile number.
+
+    Methods:
+        __str__(): Returns the string representation of the user (username).
+        get_full_name(): Returns the full name of the user, composed of first name and last name if available, otherwise username.
+        delete(): Overrides the delete method to prevent deletion of staff users.
+
+    Meta:
+        managed (bool): Indicates whether the model's table is managed by Django's migrations. Takes the value from settings.MANAGE_DATABASE.
+        db_table (str): The name of the database table for the model, set to "user".
+        verbose_name_plural (str): The plural name for the model used in the Django admin, set to "Users".
+        default_permissions (list): List of default permissions for the model, only included if settings.DEFAULT_PERMISSIONS is False.
+
+    Note:
+        This class is intended to be used as a custom user model in Django projects.
+    """
+
     USERNAME_FIELD = "username"
 
     mobile_no = CharField(unique=True, max_length=100, null=True, blank=True)
 
     def __str__(self):
+        """
+        Return the string representation of the user.
+
+        Returns:
+            str: The username of the user.
+        """
         return self.username
 
     def get_full_name(self):
+        """
+        Return the full name of the user.
+
+        If the user has both first name and last name specified, it returns the concatenation
+        of the first name and last name separated by a space. If either the first name or last name
+        is not specified, it returns the username.
+
+        Returns:
+            str: The full name of the user.
+        """
         return (
             self.username
             if not self.first_name and not self.last_name
@@ -19,11 +59,41 @@ class User(AbstractUser):
         )
 
     def delete(self, *args, **kwargs):
+        """
+        Override the delete method to prevent deletion of staff users.
+
+        This method checks if the user is a staff member. If the user is a staff member,
+        it sets the is_staff attribute to False and saves the user instance, effectively
+        removing staff privileges without deleting the user from the database.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            None
+        """
         if self.is_staff:
             self.is_staff = False
             self.save()
 
     class Meta:
+        """
+        Metadata options for the User model.
+
+        This class contains metadata options for the User model, specifying various settings
+        such as database table name, verbose name plural, managed status, and default permissions.
+
+        Attributes:
+            managed (bool): Indicates whether the model's table is managed by Django's migrations.
+            db_table (str): The name of the database table for the model.
+            verbose_name_plural (str): The plural name for the model used in the Django admin.
+            default_permissions (list): List of default permissions for the model.
+
+        Note:
+            These options define the behavior and characteristics of the User model.
+        """
+
         managed = settings.MANAGE_DATABASE
         db_table = "user"
         verbose_name_plural = "Users"
--- src/user/serializers.py
+++ src/user/serializers.py
@@ -8,24 +8,133 @@ from user.models import User
 
 
 class UserSerializer(HyperlinkedModelSerializer):
+    """
+    Serializer for the User model.
+
+    This serializer is used to serialize User objects into JSON representations
+    and vice versa, particularly for use in RESTful APIs.
+
+    Meta:
+        model (User): The user model for which the serializer is created.
+        fields (list): List of fields to be included in the serialized representation.
+
+    Attributes:
+        url (HyperlinkedIdentityField): Hyperlinked field representing the URL of the user object.
+        first_name (CharField): Field for the user's first name.
+        last_name (CharField): Field for the user's last name.
+        mobile_no (CharField): Field for the user's mobile number.
+
+    Note:
+        This serializer is tailored for the User model.
+    """
+
     class Meta:
         model = User
         fields = ["url", "first_name", "last_name", "mobile_no"]
 
 
 class GroupSerializer(HyperlinkedModelSerializer):
+    """
+    Serializer for the Group model.
+
+    This serializer is used to serialize Group objects into JSON representations
+    and vice versa, particularly for use in RESTful APIs.
+
+    Meta:
+        model (Group): The group model for which the serializer is created.
+        fields (list): List of fields to be included in the serialized representation.
+
+    Attributes:
+        url (HyperlinkedIdentityField): Hyperlinked field representing the URL of the group object.
+        name (CharField): Field for the group's name.
+
+    Note:
+        This serializer is tailored for the Group model.
+    """
+
     class Meta:
         model = Group
         fields = ["url", "name"]
 
 
 class RegistrationSerializer(ModelSerializer):
+    """
+    Serializer for user registration.
+
+    This serializer is used to handle user registration requests, including
+    validation of user input and creation of user accounts.
+
+    Attributes:
+        email (EmailField): Field for the user's email address.
+        password (CharField): Field for the user's password.
+        password2 (CharField): Field for confirming the user's password.
+        username (CharField): Field for the user's username.
+        first_name (CharField): Field for the user's first name.
+        last_name (CharField): Field for the user's last name.
+
+    Methods:
+        validate(): Method to validate the serializer's data, ensuring password fields match.
+        create(): Method to create a new user account based on validated data.
+
+    Meta:
+        model (User): The user model for which the serializer is created.
+        fields (list): List of fields to be included in the serialized representation.
+        extra_kwargs (dict): Additional keyword arguments for serializer fields.
+
+    Note:
+        This serializer is designed specifically for user registration.
+    """
+
     email = EmailField(
         required=True, validators=[UniqueValidator(queryset=User.objects.all())]
     )
 
     password = CharField(write_only=True, required=True, validators=[validate_password])
     password2 = CharField(write_only=True, required=True)
+
+    def validate(self, attrs):
+        """
+        Validate password fields.
+
+        This method ensures that the password and password2 fields match.
+
+        Args:
+            attrs (dict): The dictionary containing the serializer's data.
+
+        Returns:
+            dict: The validated data dictionary.
+
+        Raises:
+            ValidationError: If the password fields do not match.
+        """
+        if attrs["password"] != attrs["password2"]:
+            raise ValidationError({"password": "Password fields didn't match."})
+
+        return attrs
+
+    def create(self, validated_data):
+        """
+        Create a new user account.
+
+        This method creates a new user account based on the provided validated data.
+
+        Args:
+            validated_data (dict): The dictionary containing the validated serializer data.
+
+        Returns:
+            User: The newly created user object.
+        """
+        user = User.objects.create(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+        )
+
+        user.set_password(validated_data["password"])
+        user.save()
+
+        return user
 
     class Meta:
         model = User
@@ -41,23 +150,3 @@ class RegistrationSerializer(ModelSerializer):
             "first_name": {"required": True},
             "last_name": {"required": True},
         }
-
-    def validate(self, attrs):
-        if attrs["password"] != attrs["password2"]:
-            raise ValidationError({"password": "Password fields didn't match."})
-
-        return attrs
-
-    def create(self, validated_data):
-
-        user = User.objects.create(
-            username=validated_data["username"],
-            email=validated_data["email"],
-            first_name=validated_data["first_name"],
-            last_name=validated_data["last_name"],
-        )
-
-        user.set_password(validated_data["password"])
-        user.save()
-
-        return user
