8d63a1bd4947f0b0d97428578a73733e2e179d26
fiery.snowflake <extinctCoder@outlook.com>
2024-03-02 23:52:41 +0000

    Mar 3, 2024, 5:52 AM

--- start_django.sh
+++ start_django.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo -e "Strating django task management Server"
-python manage.py tailwind build
-python manage.py collectstatic --noinput
-python manage.py makemigrations
-python manage.py migrate
-python manage.py init_superuser
+python manage.py tailwind build --settings tms.settings_prod
+python manage.py collectstatic --noinput --settings tms.settings_prod
+python manage.py makemigrations --settings tms.settings_prod
+python manage.py migrate --settings tms.settings_prod
+python manage.py init_superuser --settings tms.settings_prod
 gunicorn tms.wsgi:application --bind 0.0.0.0:80 --workers 5
