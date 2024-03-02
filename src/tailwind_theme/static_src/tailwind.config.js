876144abad04a38778ef991ab6491faf8af61436
fiery.snowflake <extinctCoder@outlook.com>
2024-03-03 03:27:00 +0600

    new update

--- src/tailwind_theme/static_src/tailwind.config.js
+++ src/tailwind_theme/static_src/tailwind.config.js
@@ -1,16 +1,7 @@
-/**
- * This is a minimal config.
- *
- * If you need the full config, get it from here:
- * https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
- */
-
 module.exports = {
   content: [
     "../templates/**/*.html",
-
     "../../templates/**/*.html",
-
     "../../**/templates/**/*.html",
   ],
   theme: {
