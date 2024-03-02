ac0c572eb33fbcae3bb9ebec243536378342c5cb
fiery.snowflake <extinctCoder@outlook.com>
2024-03-02 22:11:04 +0000

    Mar 3, 2024, 4:11 AM

--- src/tms/management/commands/init_superuser.py
+++ src/tms/management/commands/init_superuser.py
@@ -7,16 +7,16 @@ from user.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if User.objects.count() == 0:
-            if settings.ADMINS != None:
-                username = settings.ADMINS["default"]["USER"]
-                email = settings.ADMINS["default"]["EMAIL"]
-                password = settings.ADMINS["default"]["PASSWORD"]
-                User.objects.create_superuser(username, email, password)
-                self.stdout.write(
-                    self.style.SUCCESS("Super user account created successfully")
-                )
-                self.stdout.write(self.style.SUCCESS(f"username \t: {username}"))
-                self.stdout.write(self.style.SUCCESS(f"password \t: {password}"))
+            # Admin Setup
+            username = "admin"
+            email = "extinctCoder@outlook.com"
+            password = "password"
+            User.objects.create_superuser(username, email, password)
+            self.stdout.write(
+                self.style.SUCCESS("Super user account created successfully")
+            )
+            self.stdout.write(self.style.SUCCESS(f"username \t: {username}"))
+            self.stdout.write(self.style.SUCCESS(f"password \t: {password}"))
 
         else:
             self.stdout.write(
