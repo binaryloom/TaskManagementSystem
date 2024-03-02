08d2ddb9a5d2709cca94943c97ddaaf70172c13b
fiery.snowflake <extinctCoder@outlook.com>
2024-03-02 21:02:53 +0000

    Mar 3, 2024, 3:02 AM

--- src/tms/settings.py
+++ src/tms/settings.py
@@ -122,14 +122,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = "static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
@@ -193,3 +185,6 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+# STATIC_HOST = "https://d4663kmspf1sqa.cloudfront.net" if not DEBUG else ""
+STATIC_URL = "static/"
