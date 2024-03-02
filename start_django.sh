9d3606c60699a2a3a454feafa12a8e5ad585f2ee
fiery.snowflake <extinctCoder@outlook.com>
2024-03-02 22:03:40 +0000

    Mar 3, 2024, 4:03 AM

--- start_django.sh
+++ start_django.sh
@@ -4,4 +4,5 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 python manage.py init_superuser
-python manage.py runserver 0.0.0.0:80 --settings tms.settings_prod
+# python manage.py runserver 0.0.0.0:80 --settings tms.settings_prod
+gunicorn djangoapp.wsgi:application --bind 0.0.0.0:80 --workers 4
