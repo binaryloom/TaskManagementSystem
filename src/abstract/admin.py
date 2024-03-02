7c03b82f78fa090289e385ab2cb1ba2263cf36aa
fiery.snowflake <extinctCoder@outlook.com>
2024-03-02 00:40:44 +0000

    Mar 2, 2024, 6:40 AM

--- src/abstract/admin.py
+++ src/abstract/admin.py
@@ -2,18 +2,17 @@ from django.conf import settings
 from django.contrib import admin, messages
 
 
-# Custom admin action to mark selected items as recovered.
 @admin.action(description="Recover Selected Items")
 def mark_undo(modeladmin, request, queryset):
     """
+    Custom admin action to mark selected items as recovered.
+
     Args:
         modeladmin: The ModelAdmin instance.
         request (HttpRequest): The HTTP request object.
         queryset (QuerySet): The queryset containing the selected items.
     """
-    # Update the 'status' field of selected items to True
     updated_count = queryset.update(status=True)
-    # Display success message to the user
     modeladmin.message_user(
         request,
         f"Successfully Recovered {updated_count} {modeladmin.model._meta.verbose_name_plural}",
@@ -32,13 +31,9 @@ class ModelAdmin(admin.ModelAdmin):
         actions (list): List of custom actions to perform on selected items in the admin interface.
     """
 
-    # Number of items to display per page in the admin list view
     list_per_page = 12
-    # List of fields to exclude from the admin interface
     exclude = ["status", "created_by", "updated_by"]
-    # List of fields to display in the admin list view
     list_display = ["__str__", "status", "created_by", "updated_by"]
-    # List of custom actions to perform on selected items in the admin interface
     actions = [mark_undo]
 
     def delete_queryset(self, request, queryset):
@@ -49,5 +44,4 @@ class ModelAdmin(admin.ModelAdmin):
             request (HttpRequest): The HTTP request object.
             queryset (QuerySet): The queryset containing the selected items to delete.
         """
-        # Soft-delete selected items by setting their 'status' field to False
         queryset.update(status=False)
