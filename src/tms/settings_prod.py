e3052b56919c4d87e582eafe5fe8e2231a834f73
fiery.snowflake <extinctCoder@outlook.com>
2024-03-03 01:42:02 +0000

    Mar 3, 2024, 7:42 AM

--- src/tms/settings_prod.py
+++ src/tms/settings_prod.py
@@ -8,23 +8,23 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 NPM_BIN_PATH = "/usr/local/bin/npm"
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": environ["APP_DB_DATABASE"],
-#         "USER": environ["APP_DB_USER"],
-#         "PASSWORD": environ["APP_DB_PASSWORD"],
-#         "HOST": environ["APP_DB_HOST"],
-#         "PORT": "5432",
-#     }
-# }
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "Wu659S",
-        "USER": "nU4Y12",
-        "PASSWORD": "RBEE5h2s88BnG2VHyMW66DSN",
-        "HOST": "127.0.0.1",
+        "NAME": environ["APP_DB_DATABASE"],
+        "USER": environ["APP_DB_USER"],
+        "PASSWORD": environ["APP_DB_PASSWORD"],
+        "HOST": environ["APP_DB_HOST"],
         "PORT": "5432",
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "Wu659S",
+#         "USER": "nU4Y12",
+#         "PASSWORD": "RBEE5h2s88BnG2VHyMW66DSN",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
