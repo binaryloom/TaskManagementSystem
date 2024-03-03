495ec9035f299f8d0f8898c68456d00e372a3f1b
fiery.snowflake <extinctCoder@outlook.com>
2024-03-03 01:24:30 +0000

    Mar 3, 2024, 7:24 AM

--- src/tms/settings_prod.py
+++ src/tms/settings_prod.py
@@ -1,6 +1,20 @@
+from os import environ
+
 from .settings import *
 
 DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 NPM_BIN_PATH = "/usr/local/bin/npm"
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "istl_imanager",
+        "USER": "istl_dbproxy",
+        "PASSWORD": "istl_dbpassword",
+        "HOST": environ["DATABASE_HOST"],
+        "PORT": "5432",
+    }
+}
