1a20ba1f71cea2f79069da279a7c5f0fab644860
fiery.snowflake <extinctCoder@outlook.com>
2024-03-04 21:11:23 +0000

    Mar 5, 2024, 3:11 AM

--- src/task_management/tests/test_urls.py
+++ src/task_management/tests/test_urls.py
@@ -5,5 +5,5 @@ from django.urls import resolve, reverse
 class TestURL(TestCase):
     def test_url_resolved(self):
 
-        print(resolve("boardlist_view"))
+        print(resolve("task_management.boardlist_view"))
         assert 1 == 1
