7217aa4bfa9fd716b8fe1828b51bc674091ba8df
fiery.snowflake <extinctCoder@outlook.com>
2024-03-05 19:43:58 +0000

    Mar 6, 2024, 1:43 AM

--- src/task_management/tests.py
+++ src/task_management/tests.py
@@ -1,9 +1,29 @@
 from django.test import TestCase
 
+from task_management.models import Board, List, Task
 
-class TestModel(TestCase):
+
+class MyTestCase(TestCase):
+    fixtures = ["myapp_fixture.json"]
+
     def setUp(self):
-        return super().setUp()
+        # Load data from the fixture into test case
+        self.obj = MyModel.objects.get(pk=1)
 
-    def test_obj(self):
-        assert 1 == 1
+    def test_fixture_data_usage(self):
+        # Use the loaded data for testing
+        self.assertEqual(self.obj.name, "Test")
+
+    # Add more test methods as needed
+
+    def tearDown(self):
+        # Remove the data after the test is executed
+        self.obj.delete()
+
+
+# class TestModel(TestCase):
+#     def setUp(self):
+#         return super().setUp()
+
+#     def test_obj(self):
+#         assert 1 == 1
