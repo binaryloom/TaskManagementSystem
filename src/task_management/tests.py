6ab540fbc6b94f17d88cb9d23f4183a388f65efe
fiery.snowflake <extinctCoder@outlook.com>
2024-03-05 20:31:55 +0000

    Mar 6, 2024, 2:31 AM

--- src/task_management/tests.py
+++ src/task_management/tests.py
@@ -1,3 +1,6 @@
+from json import load
+from os.path import join
+
 from django.conf import settings
 from django.test import TestCase
 
@@ -12,9 +15,11 @@ class TestModel(TestCase):
         self.boards = Board.objects.all()
         self.lists = List.objects.all()
         self.tasks = Task.objects.all()
+        with open(join(settings.FIXTURE_DIRS[0], self.fixtures[1]), "r") as tmp_file:
+            self.json_data = load(tmp_file)
 
     def test_board(self):
-        print(settings.FIXTURE_DIRS[0])
+        print(self.json_data)
         self.assertEqual(self.boards.count(), 1)
 
     def test_list(self):
