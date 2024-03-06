b708374627933c1e827155a6981354123df37649
fiery.snowflake <extinctCoder@outlook.com>
2024-03-06 21:50:52 +0000

    Mar 7, 2024, 3:50 AM

--- src/task_management/tests.py
+++ src/task_management/tests.py
@@ -69,10 +69,12 @@ class TestUrl(TestCase):
         self.assertEqual(tmp_object.name, tmp_form["name"])
         tmp_form = {"name": generate_str()}
         response = self.client.post(
-            reverse("boardupdate_view", kwargs={"pk": tmp_object.pk}),
+            reverse("task_management:boardupdate_view", kwargs={"pk": tmp_object.pk}),
             data=tmp_form,
         )
         self.assertEqual(response.status_code, 302)
+        tmp_object = Board.objects.last()
+        self.assertEqual(tmp_object.name, tmp_form["name"])
         response = self.client.post(
             reverse("task_management:boarddelete_view", kwargs={"pk": tmp_object.pk})
         )
